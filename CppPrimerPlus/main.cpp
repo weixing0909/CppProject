3821bf6053b396613730e18a3a45af60c36d8d55
AndrewWei <andrew354102@gmail.com>
2020-10-29 00:23:34 +0800

    add cpp primer plus

---
+++ CppPrimerPlus/main.cpp
@@ -0,0 +1,16 @@
+#include "main.h"
+
+using namespace std;
+
+int main(int argc, char** argv)
+{
+    cout << "entry cpp primer plus practice" << endl;
+	int index = 1;
+	switch (index)
+	{
+	case 1: chapter_01(); break;
+	default:break;
+	}
+	cout << "quit cpp primer plus practice" << endl;
+    return 0;
+}
---
+++ CppPrimerPlus/main.h
@@ -0,0 +1,8 @@
+#ifndef __MAIN_H__
+#define __MAIN_H__
+
+#include<iostream>
+
+void chapter_01();
+
+#endif
