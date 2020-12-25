ce83e32ee30805adbdbd96df936d083158560e30
AndrewWei <andrew354102@gmail.com>
2020-12-25 14:14:49 +0800

    shell

---
+++ shell_script/make_dir.sh
@@ -0,0 +1,17 @@
+mkdir -p practice
+mkdir -p prj
+mkdir -p src
+mkdir -p src/01_android
+mkdir -p src/02_dataset
+mkdir -p src/03_lib
+mkdir -p src/04_framework
+mkdir -p src/04_mobile_framework
+mkdir -p src/05_tool
+mkdir -p src/10_pytorch
+mkdir -p src/30_reid
+mkdir -p src/31_obj_detect
+mkdir -p src/32_classification
+mkdir -p src/33_pose
+mkdir -p src/40_mmlab
+mkdir -p src/60_tutorial
+mkdir -p src/99_fun
